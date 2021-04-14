9846d1a1e39052ef48c52ec29c6a7bb359b15b40
Sam Ito <isamsito@gmail.com>
2021-04-13 20:29:19 -0600

    rename google-maps-search file (fix spelling)

--- express-back-end/google-maps-search.js
+++ express-back-end/google-maps-search.js
@@ -2,7 +2,7 @@ const needle = require('needle');
 require('dotenv').config()
 // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
-modules.exports = getLatLngFromLocation = function(location) {
+const getLatLngFromLocation = function(location) {
   return new Promise((resolve, reject) => {
     needle.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_MAPS_GEOCODE_API_KEY}`, (error, response) => {
       if (error) reject(error);
@@ -15,4 +15,4 @@ modules.exports = getLatLngFromLocation = function(location) {
   }) 
 }
 
-
+module.exports = { getLatLngFromLocation }
