aaf6727c03575efec70e4039e9638ee5657fe445
Sam Ito <isamsito@gmail.com>
2021-04-13 20:33:20 -0600

    Add heat map

--- react-front-end/src/components/Map/Map.js
+++ react-front-end/src/components/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker, GoogleApiWrapper, HeatMap } from "google-maps-react";
 import './Map.scss';
 import mapStyle from './mapStyle';
 import Header from '../Header/Header'
@@ -10,10 +10,50 @@ export class MapContainer extends Component {
     map.setOptions({
       styles: mapStyle
     });
+    this.tweetPositions = mapProps.tweetPositions;
   }
+
+  // onMapClick({x, y, lat, lng, event}) {
+  //   if (this._googleMap !== undefined) {
+  //     const point = new google.maps.LatLng(lat, lng)
+  //     this._googleMap.heatmap.data.push(point)
+  //   }
+  // }
+
+  
 
   render() {
     const coords = { lat: 49.279793, lng: -123.115669 };
+    const heatMapData = {
+      positions: [
+        {lat: 49.2827, lng:-123.1217},
+        {lat: 49.2828, lng:-123.1227},
+        {lat: 49.2829, lng:-123.1237},
+        {lat: 49.2830, lng:-123.1247},
+        {lat: 49.2927, lng:-123.157}
+      ],
+      options: {   
+        radius: 20,   
+        opacity: 0.6,
+        weight: 20
+      }
+    }
+    const gradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ];
     return (
       <Map
         className="map"
@@ -26,11 +66,18 @@ export class MapContainer extends Component {
       >
         <Marker position={coords} />
         <Header />
+        <HeatMap
+          gradient={gradient}
+          opacity={0.2}
+          positions={heatMapData.positions}
+          radius={10}
+        />
       </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_MAPS_KEY
+  apiKey: process.env.REACT_APP_MAPS_KEY,
+  libraries: ["visualization"]
 })(MapContainer);
