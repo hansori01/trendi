b9bfad0cd5d0c90edd083f1e85c683efd718fa3e
hansori01 <19sorihan88@gmail.com>
2021-04-15 16:06:22 -0700

    skeleton for TweetStateProvider before working back end people

---
+++ react-front-end/src/components/States/TweetStateProvider.js
@@ -0,0 +1,17 @@
+import { createContext, useState, useEffect } from 'react';
+
+
+export default function TweetStateProvider(props) {
+
+
+  // const tweetData = {
+
+  // };
+
+  return (
+    // <tweetContext.Provider value={tweetData}>
+    //   {props.children}
+    // </tweetContext.Provider>
+  );
+};
+// export const tweetContext = createContext();
