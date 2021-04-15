611f82632ef2281e62c4441d32f871d7d6cdc6ff
Sam Ito <isamsito@gmail.com>
2021-04-15 11:32:36 -0600

    heatmap sort of working with constant data

--- react-front-end/src/components/Map/Map.js
+++ react-front-end/src/components/Map/Map.js
@@ -1,13 +1,55 @@
 import React from "react";
-import { GoogleMap, OverlayView, HeatmapLayer } from "react-google-maps";
+import { GoogleMap, OverlayView } from "react-google-maps";
+import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer';
 import './Map.scss';
 import './mapStyle';
 import Header from '../Header/Header';
 import withGoogleMap from "react-google-maps/lib/withGoogleMap";
 import withScriptjs from "react-google-maps/lib/withScriptjs";
 import mapStyle from "./mapStyle";
+/*global google*/
 require('dotenv').config()
 
+
+// {lat: 49.2827, lng: -123.1207},
+//   {lat: 49.2927, lng: -123.1307},
+//   {lat: 49.3027, lng: -123.1407},
+//   {lat: 49.3127, lng: -123.1507},
+//   {lat: 49.3227, lng: -123.1607},
+//   {lat: 49.3327, lng: -123.1707},
+// const google = window.google
+const p1 = new google.maps.LatLng(49.2827, -123.1207);
+const p2 = new google.maps.LatLng(49.2927, -123.1307);
+const p3 = new google.maps.LatLng(49.3027, -123.1407);
+const p4 = new google.maps.LatLng(49.3127, -123.1507);
+const p5 = new google.maps.LatLng(49.3227, -123.1607);
+const p6 = new google.maps.LatLng(49.3327, -123.1707);
+
+const points = [
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+];
+const gradient = [
+  'rgba(0, 255, 255, 0)',
+  'rgba(0, 255, 255, 1)',
+  'rgba(0, 191, 255, 1)',
+  'rgba(0, 127, 255, 1)',
+  'rgba(0, 63, 255, 1)',
+  'rgba(0, 0, 255, 1)',
+  'rgba(0, 0, 223, 1)',
+  'rgba(0, 0, 191, 1)',
+  'rgba(0, 0, 159, 1)',
+  'rgba(0, 0, 127, 1)',
+  'rgba(63, 0, 91, 1)',
+  'rgba(127, 0, 63, 1)',
+  'rgba(191, 0, 31, 1)',
+  'rgba(255, 0, 0, 1)'
+];
+console.log("Points: ", points);
 const ReactMap = withScriptjs(withGoogleMap((props) =>
 
 <div className="map">
@@ -20,6 +62,7 @@ const ReactMap = withScriptjs(withGoogleMap((props) =>
     defaultCenter={{ lat: 49.279793, lng: -123.115669 }}
     clickableIcons={false}
   >
+    <HeatmapLayer data={points} opacity={1} radius={1000} gradient={gradient} />
   </GoogleMap>
 </div>
 
