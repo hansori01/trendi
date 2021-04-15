bd6ea32afcc94d164a84de0ae705bd2e86820a0d
hansori01 <19sorihan88@gmail.com>
2021-04-15 16:15:44 -0700

    commented out useCOntext code placed to help going forward with refactoring

--- react-front-end/src/components/Map/Map.js
+++ react-front-end/src/components/Map/Map.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+// import {tweetContext} from '../States/TweetStateProvider'
 import { GoogleMap, OverlayView, HeatmapLayer } from "react-google-maps";
-import './Map.scss';
-import './mapStyle';
 import withGoogleMap from "react-google-maps/lib/withGoogleMap";
 import withScriptjs from "react-google-maps/lib/withScriptjs";
 import mapStyle from "./mapStyle";
+import './Map.scss';
+import './mapStyle';
+
 require('dotenv').config()
+
+// const {} = useContext(tweetContext)
 
 const ReactMap = withScriptjs(withGoogleMap((props) =>
 
-    <GoogleMap
-      defaultOptions={{
-        styles: mapStyle,
-        disableDefaultUI: true,
-      }}
-      zoom={10}
-      defaultCenter={{ lat: 49.279793, lng: -123.115669 }}
-      clickableIcons={false}
-    >
-    </GoogleMap>
+  <GoogleMap
+    defaultOptions={{
+      styles: mapStyle,
+      disableDefaultUI: true,
+    }}
+    zoom={10}
+    defaultCenter={{ lat: 49.279793, lng: -123.115669 }}
+    clickableIcons={false}
+  >
+  </GoogleMap>
 
 ));
 
--- react-front-end/src/components/States/TweetStateProvider.js
+++ react-front-end/src/components/States/TweetStateProvider.js
@@ -1,17 +1,17 @@
 import { createContext, useState, useEffect } from 'react';
 
 
-export default function TweetStateProvider(props) {
+// export default function TweetStateProvider(props) {
 
-
+//export functions / state here
   // const tweetData = {
 
   // };
 
-  return (
-    // <tweetContext.Provider value={tweetData}>
-    //   {props.children}
-    // </tweetContext.Provider>
-  );
-};
+  // return (
+  //   <tweetContext.Provider value={tweetData}>
+  //     {props.children}
+  //   </tweetContext.Provider>
+  // );
+// };
 // export const tweetContext = createContext();
