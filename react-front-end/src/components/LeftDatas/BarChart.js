d741155e0d10edc75bc7d3afde6ec317ee15f5ed
Joshua Rully <joshrully@gmail.com>
2021-04-16 12:46:16 -0700

    implement useState and useEffect to properly detect score changes

--- react-front-end/src/components/LeftDatas/BarChart.js
+++ react-front-end/src/components/LeftDatas/BarChart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {tweetContext} from '../States/TweetStateProvider'
 
 import { BarChart, XAxis, Bar } from 'recharts';
@@ -10,24 +10,37 @@ export default function SentiBarChart() {
 
   // tweets.map(e => console.log('incoming socre! =>', e.sentiment.score));
   
-  const tweetScores = [
-    //negative numbers are 11-15 because im lazy rn
-    { name: "3-", 0: 0 },
-    { name: "2-", 0: 0 },
-    { name: "1-", 0: 0 },
-    { name: "0", 0: 0 },
-    { name: "1", 0: 0 },
-    { name: "2", 0: 0 },
-    { name: "3", 0: 0 },
-  ]
+  const [tweetScores, setTweetScores] = useState({
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0
+  })
 
-  tweetScores[3][0] = tweets[0].sentiment.score;
+  useEffect(() => {
+    // if tweets[0].sentiment.score === 0
+    //   score = d;
+    setTweetScores(prevScores => ({...prevScores, score: +1}))
+  }, [tweets])
+
+  const barData = [
+    { name: "3-", 0: tweetScores.a },
+    { name: "2-", 0: tweetScores.b },
+    { name: "1-", 0: tweetScores.c },
+    { name: "0", 0: tweetScores.d },
+    { name: "1", 0: tweetScores.e },
+    { name: "2", 0: tweetScores.f },
+    { name: "3", 0: tweetScores.g },
+  ];
 
   return (
     <BarChart
       width={420}
       height={280}
-      data={tweetScores}
+      data={barData}
       margin={{
         top: 20,
         right: 10,
