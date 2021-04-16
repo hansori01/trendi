42f89cace26d4d8ebef5e05e0eeb5add87e651c5
hansori01 <19sorihan88@gmail.com>
2021-04-16 10:45:40 -0700

    adding mat-ui buttons before styling

--- react-front-end/src/components/Header/Header.js
+++ react-front-end/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { uiContext } from '../States/UIStateProvider'
-
 import HoverImage from "react-hover-image";
+import Button from '@material-ui/core/Button'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
-import './Header.scss';
 import ChooseCountry from './ChooseCountry';
 import TrendingHash from './TrendingHash';
+import './Header.scss';
 import { Animated } from "react-animated-css";
 
 
@@ -25,6 +28,18 @@ export default function Header(props) {
           src="./images/logo.png"
           hoverSrc="./images/logohover.png"
           className="logo" />
+        <span className='controllerButtons' >
+          <Button className="button start">
+            <PlayCircleOutlineIcon />
+          </Button>
+          <Button className="button pause">
+            <PauseCircleOutlineIcon />
+          </Button>
+          <Button className="button stop">
+            <HighlightOffIcon />
+          </Button>
+        </span>
+
         <img
           src='./images/user.png'
           alt=''
