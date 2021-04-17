8320739406d5e221e6e0906c3c962363f1a78558
Sam Ito <isamsito@gmail.com>
2021-04-17 14:54:45 -0600

    Use regex to parse negative and positive arrays from tweets and form words lists

--- react-front-end/src/components/LeftDatas/LeftData.js
+++ react-front-end/src/components/LeftDatas/LeftData.js
@@ -1,16 +1,48 @@
 import React, { useContext } from "react";
+
 // import {tweetContext} from '../States/TweetStateProvider'
 
-import words from './tempWords';
+// import words from './tempWords';
 import Wordcloud from './Wordcloud';
 import SentiBarChart from './BarChart';
 import SentiPieChart from './PieChart';
+import data from "../../temp-data/seedData-location-added"
 
 import './LeftData.scss';
 
 export default function LeftData() {
   
   // const {} = useContext(tweetContext)
+  const positiveTweets = data.filter((tweet) => tweet.sentiment.score > 0);
+  const negativeTweets = data.filter((tweet) => tweet.sentiment.score < 0);
+  const neutralTweets = data.filter((tweet) => tweet.sentiment.score === 0);
+  const tweets = data;
+  
+  const words = [];
+  const getWord = function(wordText) {
+    return words.findIndex((word) => word.text===wordText);
+  }
+
+  const pushToWordsArray = function(array, isPositive) {
+    array.forEach(word => {
+      // if the object already exists, update the value of the word
+      let wordIndex = getWord(word)
+      if (wordIndex > -1) {
+        words[wordIndex].value++;
+      } else {
+        //if the object does not exist push to words'
+        words.push({ text: word, value: 1, sentiment: isPositive})
+      }
+    });
+  }
+  
+  data.forEach(tweet => {
+    let negativeWordsArr = tweet.sentiment.negative.split(/(?:,|\[|\])/gm).slice(1,-1);
+    let positiveWordsArr = tweet.sentiment.positive.split(/(?:,|\[|\])/gm).slice(1,-1);
+    pushToWordsArray(negativeWordsArr, false);
+    pushToWordsArray(positiveWordsArr, true);
+  })
+
 
   return (
     <section className="leftData">
