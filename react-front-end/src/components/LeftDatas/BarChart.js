97ec137e3e8c1bb110b50e1d81f8478a58663dfe
Joshua Rully <joshrully@gmail.com>
2021-04-16 18:18:06 -0700

    fully functional and tested at low volume

--- react-front-end/src/components/LeftDatas/BarChart.js
+++ react-front-end/src/components/LeftDatas/BarChart.js
@@ -8,8 +8,6 @@ import './LeftData.scss';
 export default function SentiBarChart() {
   const { tweets } = useContext(tweetContext)
 
-  
-  
   const [tweetScores, setTweetScores] = useState({
     veryNeg: 0,
     neg: 0,
@@ -21,14 +19,11 @@ export default function SentiBarChart() {
   })
 
   // access previous state scores
-  // spread then increment values depending on sentiment
+  // spread, then increment values depending on sentiment value
+  // this hurt my brain beyond comprhension ~ monke no good w/ numbies
   useEffect(() => {
 
     setTweetScores(prevScores => {
-      // logs below for debugging
-      console.log('these are the tweets for the scores below!', tweets)
-      tweets.map(e => console.log('incoming score! =>', e.sentiment.score));
-
       const newData = {
         ...prevScores
       };
@@ -37,41 +32,27 @@ export default function SentiBarChart() {
         const sentiScore = tweets[0].sentiment.score;
         console.log(sentiScore);
         if ((sentiScore > -1 && sentiScore < 1) || sentiScore === 0) {
-          newData.neutral = prevScores.neutral + 1;
+          newData.neutral = prevScores.neutral + 1;         //  0
         } else if (sentiScore >= 1 && sentiScore < 2) {
-          newData.slightPos = prevScores.slightPos + 1;
+          newData.slightPos = prevScores.slightPos + 1;     //  3
         } else if (sentiScore >= 2 && sentiScore < 3) {
-          newData.pos = prevScores.pos + 1;
+          newData.pos = prevScores.pos + 1;                 //  2
         } else if (sentiScore >= 3) {
-          newData.veryPos = prevScores.veryPos + 1;
+          newData.veryPos = prevScores.veryPos + 1;         //  1
         } else if (sentiScore <= -1 && sentiScore > -2) {
-          newData.slightNeg = prevScores.slightNeg + 1;
+          newData.slightNeg = prevScores.slightNeg + 1;     // -1
         } else if (sentiScore <= -2 && sentiScore > -3) {
-          newData.neg = prevScores.neg + 1;
+          newData.neg = prevScores.neg + 1;                 // -2
         } else if (sentiScore <= -3) {
-          newData.veryNeg = prevScores.veryNeg + 1;
+          newData.veryNeg = prevScores.veryNeg + 1;         // -3
         } else {
-          console.log("This score doesn't have a home!",sentiScore)
+          console.log("This score doesn't have a home: ", sentiScore)
         }
       }
 
-      // prev sentiment score states
-      const prevVeryNeg = prevScores.veryNeg // 3-
-      const prevNeg = prevScores.neg // 2-
-      const prevSlightNeg = prevScores.slightNeg // 1-
-      const prevNeutral = prevScores.neutral; // 0
-      const prevSlightPos = prevScores.slightPos // 1
-      const prevPos = prevScores.pos // 2
-      const prevVeryPos = prevScores.veryPos; // 3
-      
-      console.log(newData);
-
       return(
-        // {
-        //   ...prevScores,
-        //   neutral: prevNeutral + 1, 
-        // }
-        newData // this will contain the prevScores and any new updated data
+        // this will contain the prevScores and any new updated data
+        newData 
       )
     })
   }, [tweets])
