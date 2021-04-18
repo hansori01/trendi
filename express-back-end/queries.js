f9feb7e28d4d76ef147b11485134cec812e2d28f
Sam Ito <isamsito@gmail.com>
2021-04-18 16:37:41 -0600

    remove unused functions and comments

--- express-back-end/queries.js
+++ express-back-end/queries.js
@@ -21,9 +21,6 @@ const headers = {
   Authorization: `Bearer ${process.env.BEARER_TOKEN}`
 }
 
-const calgaryPointRadius = '51.0447,-114.0719,100mi';
-const torontoPointRadius = '43.6532,-79.3832,50mi';
-
 
 // This works, basic search for word
 const streamKeyword = function(searchWord) {
@@ -45,39 +42,6 @@ const streamCanadaBorderBox = function(searchWord) {
   });
 
   return stream;
-}
-
-
-const streamUSBorderBox = function(searchWord) {
-  const USA = ['-171.791110603', '18.91619', '-66.96466', '71.3577635769'];
-
-  const regexpression = searchWord
-  const regex = new RegExp(regexpression, "gi");
-
-  const stream = T.stream('statuses/filter', {
-    locations: USA,
-    track: '#Trump',
-    language: 'en'
-  });
-  stream.on('tweet', async tweet => {
-    if(tweet.text.match(regex)){
-      console.log(tweet);
-    }
-    console.log(tweetCount);
-      tweetCount++;
-  });
-}
-
-
-const getTweetsFromPointRadius = function(pointRadius) {
-  T.get('search/tweets', { q:`#RemoveThePM geocode:${pointRadius}`, count: 10 }, function(err, data, response) {
-    console.log(data)
-  
-    console.log('######################################################################');
-    data.statuses.forEach(tweet => {
-      console.log(tweet.user);
-    })
-  })
 }
 
 
@@ -105,62 +69,5 @@ const getCurrentUSATrends = function() {
     });
   })
 }
-
-// const canada = ['-140.99778', '41.6751050889', '-52.6480987209', '83.23324'];
-// let stream = T.stream('statuses/filter', {
-//   track: '#RemoveThePM',
-//   language: 'en'
-// });
-// stream.on('tweet', async tweet => {
-//   console.log('########################### NEW TWEET ###########################');
-//     console.log(tweet.text);
-//     if (tweet.user){
-//       console.log('User location: ', tweet.user.location);
-//     }
-//     if (tweet.place) {
-//       console.log('_______________Place data__________________');
-//       console.log(tweet.place)
-//     }
-//     console.log('######################################################################');
-// });
-
-// streamCanadaBorderBox('#RemoveThePM');
-// streamUSBorderBox('#Obama');
-
-// const runSingleQuery = function() {
-//   const headers = {
-//     Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-//   }
-//   needle.get('https://api.twitter.com/1.1/search/tweets.json?q=%23RemoveThePM%20-filter%3Aretweets%20AND%20-filter%3Areplies&geocode=48.428421,-123.365646,30km',{headers: headers}, function(error, response) {
-//     if (!error && response.statusCode == 200)
-//     console.log(response.body);
-//   });
-// }
-
-const senti = new Sentiment();
-
-const runSingleQuery = function(hashtag) {
-  const headers = {
-    Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-  }
-  needle.get(`https://api.twitter.com/1.1/search/tweets.json?q=%23${hashtag}%20-filter%3Aretweets%20AND%20-filter%3Areplies&geocode=${torontoPointRadius}`,{headers: headers}, function(error, response) {
-    if (!error && response.statusCode == 200)
-    console.log(response.body);
-    console.log(response.body.user);
-    console.log('##############################################################################')
-    response.body.statuses.forEach(status => {
-      // console.log(status.user);
-      console.log(status.user);
-      console.log(senti.analyze(status.text));
-    })
-  });
-}
-
-
-// runSingleQuery('NDPConvention2021');
-// streamCanadaBorderBox('#NDPConvention2021');
-// getCurrentUSATrends()
-// runSingleQuery('NYTimesPropaganda');
-// streamKeyword('#TRUANON');
 
 module.exports = { streamCanadaBorderBox, getCurrentCanadaTrends, getCurrentUSATrends }
